af5c975ad24b29dcbabf5949ff9069a94b837cc5
joao <69046963+JoaoBr28@users.noreply.github.com>
2022-05-24 18:28:58 -0300

    first commit

---
+++ src/br/dio/pkg/primeiro/model/gato.java
@@ -0,0 +1,63 @@
+package br.dio.pkg.primeiro.model;
+
+import java.util.Objects;
+
+public class gato {
+    private  String nome ;
+    private String idade;
+    private String cor ;
+
+    public gato() { }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public gato(String nome, String cor, String idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        gato gato = (gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public String toString() {
+        return "gato{" +
+                "nome='" + nome + '\'' +
+                ", idade='" + idade + '\'' +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+
+   // @Override
+  //  public int hashCode() {
+    //    return Objects.hash(nome, cor, idade, idade);
+    //}
+}
---
+++ src/br/dio/pkg/primeiro/progama.java
@@ -0,0 +1,11 @@
+package br.dio.pkg.primeiro;
+
+public class progama {
+    public static  void main (String[]args){
+        //Gato gato = new Gato();
+       // System out;Println(gato);
+         //int a = 5;
+        //int b = 3; /
+        //System.out.println("hello world"+ (a+b));
+    }
+}
